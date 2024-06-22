d424d8ac46c0903e4a0d33baca686768f5419222
ANANDAKRISHNAN-MS <i.am.anandakrishnan.m.s@gmail.com>
2024-06-22 20:40:53 +0530

    Add ping message adding server command line functionality

--- src/com/nandu/chat_project/server/Server.java
+++ src/com/nandu/chat_project/server/Server.java
@@ -7,15 +7,21 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Server implements Runnable{
 	
 	private List<ServerClient> clients = new ArrayList<ServerClient>();
+	private List<Integer> clientResponse = new ArrayList<Integer>();
 	
 	private int port;
 	private DatagramSocket socket;
 	private boolean running = false;
 	private Thread run,manage,send,receive;
+	
+	private boolean raw = false;
+	
+	private final int MAX_ATTEMPTS = 5;
 	
 	public Server(int port) {
 		this.port=port;
@@ -35,12 +41,54 @@ public class Server implements Runnable{
 		running = true;
 		manageClients();
 		recieve();	
+		
+		Scanner scanner = new Scanner(System.in);
+		while(running) {
+			String text = scanner.nextLine();
+			if(!text.startsWith("/")) {
+				
+			}
+			text=text.substring(1);
+			if(text.equals("raw")) {
+				raw=!raw;
+			}else if(text.equals("clients")) {
+				System.out.println("Clients");
+				System.out.println("--------------");
+				for(int i =0;i<clients.size();i++) {
+					ServerClient c  =ion clients.get(i);
+					System.out.println(c.name + " (" + c.getID() + ") : " + c.address.toString() + ":" + c.port);
+				}
+				System.out.println("--------------");
+			}
+			
+				
+		}
 	}
 	
 	private void manageClients() {
 		manage = new Thread("Manage") {
 			public void run() {
 				while(running) {
+					sendToAll("/p/server");
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					for(int i =0;i<clients.size();i++) {
+						ServerClient c = clients.get(i);
+						if(!clientResponse.contains(c.getID())) {
+							if(c.attempt >= MAX_ATTEMPTS) {
+								disconnect(c.getID(),false);
+								
+							}else {
+								c.attempt++;
+							}
+						}else {
+							clientResponse.remove(new Integer(c.getID()));
+							c.attempt=0;
+						}
+					}
 					
 				}
 			}
@@ -67,6 +115,13 @@ public class Server implements Runnable{
 	}
 	
 	private void sendToAll(String message) {
+		
+		if(message.startsWith("/m/")) {
+			String text = message.substring(3);
+			text=text.split("/e/")[0];
+			System.out.println(text);
+		}
+	
 		for(int i=0;i<clients.size();i++) {
 			ServerClient client = clients.get(i);
 			send(message.getBytes(),client.address,client.port);
@@ -93,7 +148,10 @@ public class Server implements Runnable{
 	}
 
 	private void process(DatagramPacket packet) {
+	
 		String string = new String(packet.getData());
+		
+		if(raw) System.out.println(string);
 		if(string.startsWith("/c/")) {
 			int id  = UniqueIdentifier.getIdentifier();
 			clients.add(new ServerClient(string.substring(3,string.length()),packet.getAddress(),packet.getPort(),id));
@@ -105,6 +163,9 @@ public class Server implements Runnable{
 		}else if (string.startsWith("/d/")) {
 			String id = string.split("/d/|/e/")[1];
 			disconnect(Integer.parseInt(id),true);
+			
+		}else if(string.startsWith("/p/")) {
+			clientResponse.add(Integer.parseInt(string.split("/p/|/e/")[0]));
 			
 		}
 	}
